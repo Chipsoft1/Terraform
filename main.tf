99a6ff2fda765884917f728059be2e688e457b82
Chipsoft1 <z.kadioglu@chipsoft.com>
2024-05-21 15:06:37 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "null_resource" "provision" {
 
   provisioner "remote-exec" {
     inline = [
-      "powershell.exe -Command \"@\"\n${var.powershell_script_content}\n@ | Out-File -FilePath 'C:\\Scripts\\ScriptT1.ps1' -Encoding utf8\"",
+      "powershell.exe -Command \"@'\n${var.powershell_script_content}\n'@ | Set-Content -Path 'C:\\Scripts\\ScriptT1.ps1' -Encoding utf8\"",
       "powershell.exe -ExecutionPolicy Bypass -File C:\\Scripts\\ScriptT1.ps1"
     ]
   }
