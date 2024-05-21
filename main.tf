8104016ae84c240ba2bd02688a529c861023caf0
Chipsoft1 <z.kadioglu@chipsoft.com>
2024-05-21 11:36:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,71 +2,12 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "East US"
-}
-
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_windows_virtual_machine" "example" {
-  name                = "example-machine"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  size                = "Standard_DS1_v2"
-  admin_username      = "beheerder"
-  admin_password      = "M@sterY0D@123"
-
-  network_interface_ids = [
-    azurerm_network_interface.example.id,
-  ]
-
-  os_disk {
-    name              = "example-os-disk"
-    caching           = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
-    version   = "latest"
-  }
-}
-
 resource "null_resource" "provision" {
-  depends_on = [azurerm_windows_virtual_machine.example]
-
   connection {
     type     = "winrm"
-    host     = azurerm_windows_virtual_machine.example.private_ip_address
+    host     = "4.180.155.203"
     user     = "beheerder"
-    password = "M@sterY0D@123"
+    password = "M@sterY0d@123"
     https    = false
     port     = 5985
   }
@@ -78,8 +19,9 @@ resource "null_resource" "provision" {
 
   provisioner "remote-exec" {
     inline = [
-      "powershell.exe -Command \"& {Start-Sleep -Seconds 30}\"", # Wait for the machine to be ready
       "powershell.exe -File C:\\setup.ps1"
     ]
   }
 }
+
+
