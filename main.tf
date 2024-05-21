9dc70100c8a6e1b65da40f9ac9eb10e391eec205
Chipsoft1 <z.kadioglu@chipsoft.com>
2024-05-21 12:06:35 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ resource "null_resource" "provision" {
   }
 
   provisioner "file" {
-    source      = "scripts/Script.ps1"
-    destination = "C:\\Script.ps1"
+    source      = "C:\Users\beheerder\terraform-azure-windows\scripts\Script.ps1"
+    destination = "C:\Users\beheerder\terraform-azure-windows\scripts"
   }
 
   provisioner "remote-exec" {
